12ac83b126445176cc4e0bf007b2961582e0ff1c
MakoDoli <makodolidze@hotmail.com>
2024-07-05 14:21:50 +0400

    removed unneeded proccess.on-s from server.ts

--- dist/server.js
+++ dist/server.js
@@ -5,20 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 3000;
-const server = app_1.default.listen(PORT, () => {
+app_1.default.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-process.on('uncaughtException', (err) => {
-    console.log(err.name, err.message);
-    console.log('Uncaught exception occured');
-    server.close(() => {
-        process.exit(2);
-    });
-});
-process.on('unhandledRejection', (err) => {
-    console.log(err.name, err.message);
-    console.log('Unhandled rejection occured! Shutting down');
-    server.close(() => {
-        process.exit(1);
-    });
-});
+// process.on('uncaughtException', (err) => {
+//   console.log(err.name, err.message);
+//   console.log('Uncaught exception occured');
+//   server.close(() => {
+//     process.exit(2);
+//   });
+// });
+// process.on('unhandledRejection', (err: Error) => {
+//   console.log(err.name, err.message);
+//   console.log('Unhandled rejection occured! Shutting down');
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
--- src/server.ts
+++ src/server.ts
@@ -2,24 +2,24 @@ import app from './app';
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-process.on('uncaughtException', (err) => {
-  console.log(err.name, err.message);
-  console.log('Uncaught exception occured');
+// process.on('uncaughtException', (err) => {
+//   console.log(err.name, err.message);
+//   console.log('Uncaught exception occured');
 
-  server.close(() => {
-    process.exit(2);
-  });
-});
+//   server.close(() => {
+//     process.exit(2);
+//   });
+// });
 
-process.on('unhandledRejection', (err: Error) => {
-  console.log(err.name, err.message);
-  console.log('Unhandled rejection occured! Shutting down');
+// process.on('unhandledRejection', (err: Error) => {
+//   console.log(err.name, err.message);
+//   console.log('Unhandled rejection occured! Shutting down');
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
